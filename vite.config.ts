26abbbf72abfa9c502374dd5f919282fe761c2d6
erikbg7 <erik.bg7@gmail.com>
2022-11-07 19:48:22 +0100

    ♻️ Improve folder structure

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: "./ui-src",
+  root: "./ui",
   plugins: [reactRefresh(), viteSingleFile()],
   build: {
     target: "esnext",
