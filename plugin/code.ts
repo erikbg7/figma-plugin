4eb5b5c4cbf06325cb3fafa51f3670c168c3d591
erikbg7 <erik.bg7@gmail.com>
2022-11-09 14:57:28 +0100

    :sparkles: Append remote image to Figma node

--- plugin/code.ts
+++ plugin/code.ts
@@ -12,98 +12,56 @@ figma.showUI(__html__, { height: 400, width: 300 });
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = (msg) => {
-  // Update selected nodes content with the text clicked in the UI
-  if (msg.type === 'update-nodes') {
-    const nodes = figma.currentPage.selection;
-    const node = nodes[0];
-    // nodes.forEach((node) => {})
-    console.log({ node });
+  const nodes = figma.currentPage.selection;
+  const node = nodes[0];
 
-    // If the selected node is a text node, copy the payload text to it
+  // Update a selected text node with the text clicked in the UI.
+  // If the selected node is a text node, copy the payload text to it.
+  if (msg.type === 'update-text-node') {
     if (isTextNode(node) && msg.text) {
       const font = isValidFontName(node.fontName)
         ? { family: node.fontName.family, style: node.fontName.style }
         : { family: 'Inter', style: 'Medium' };
 
       figma.loadFontAsync(font).then(() => (node.characters = msg.text));
-    } else {
-      invertIfApplicable(node).then(() => {
-        console.log('done!');
-        figma.closePlugin();
-      });
+    }
+  }
+
+  // Update a selected image node with the text clicked in the UI.
+  // Look for fills on node types that have fills.
+  // Create a new array of fills, because we can't directly modify the old one
+  if (msg.type === 'update-image-node') {
+    if (isValidImageNode(node) && msg.bytes) {
+      const newFills = [];
+      const imageNode = node as GeometryMixin;
+      const imageBytes = msg.bytes as Uint8Array;
+
+      for (const paint of imageNode.fills as Paint[]) {
+        newFills.push({
+          type: 'IMAGE',
+          scaleMode: 'FILL',
+          scalingFactor: 0.5,
+          imageHash: figma.createImage(imageBytes).hash,
+        } as Paint);
+      }
+      imageNode.fills = newFills;
     }
   }
 };
+
+///////////////////////////////////////////////
+//////////////////// UTILS ////////////////////
+///////////////////////////////////////////////
 
 function isTextNode(node: SceneNode): node is TextNode {
   return (node as TextNode).characters !== undefined;
 }
 
+function isValidImageNode(node: SceneNode): boolean {
+  const validTypes = ['RECTANGLE', 'ELLIPSE', 'POLYGON', 'STAR', 'VECTOR', 'TEXT'];
+  return !!validTypes.find((t) => t === node.type);
+}
+
 function isValidFontName(fontName: TextNode['fontName']): fontName is FontName {
   return (fontName as FontName).family !== undefined && (fontName as FontName).style !== undefined;
-}
-
-// function isTexNode(node: SceneNode): node is TextNode {
-//   return (node as Node).characters !== undefined;
-// }
-
-async function invertPaint(paint: Paint) {
-  // Only invert the color for images (but you could do it
-  // for solid paints and gradients if you wanted)
-  if (paint.type === 'IMAGE' && paint.imageHash) {
-    // Paints reference images by their hash.
-    const image = figma.getImageByHash(paint.imageHash);
-
-    // Get the bytes for this image. However, the "bytes" in this
-    // context refers to the bytes of file stored in PNG format. It
-    // needs to be decoded into RGBA so that we can easily operate
-    // on it.
-    const bytes = await image?.getBytesAsync();
-
-    // Decoding to RGBA requires browser APIs that are only available
-    // within an iframe. So we create an invisible iframe to act as
-    // a "worker" which will do the task of decoding and send us a
-    // message when it's done. This worker lives in `decoder.html`
-    figma.showUI(__html__, { visible: false });
-
-    // Send the raw bytes of the file to the worker
-    figma.ui.postMessage(bytes);
-
-    // Wait for the worker's response
-    const newBytes: Uint8Array = await new Promise((resolve, reject) => {
-      figma.ui.onmessage = (value) => resolve(value as Uint8Array);
-    });
-
-    // Create a new paint for the new image. Uploading the image will give us
-    // an image hash.
-    const newPaint = JSON.parse(JSON.stringify(paint));
-    newPaint.imageHash = figma.createImage(newBytes).hash;
-    return newPaint;
-  }
-  return paint;
-}
-
-async function invertIfApplicable(node: SceneNode) {
-  // Look for fills on node types that have fills.
-  // An alternative would be to do `if ('fills' in node) { ... }
-  switch (node.type) {
-    case 'RECTANGLE':
-    case 'ELLIPSE':
-    case 'POLYGON':
-    case 'STAR':
-    case 'VECTOR':
-    case 'TEXT': {
-      // Create a new array of fills, because we can't directly modify the old one
-      const newFills = [];
-      for (const paint of node.fills as Paint[]) {
-        newFills.push(await invertPaint(paint));
-      }
-      node.fills = newFills;
-      break;
-    }
-
-    default: {
-      // not supported, silently do nothing
-    }
-  }
 }
--- ui/components/TreeLeaves.tsx
+++ ui/components/TreeLeaves.tsx
@@ -4,14 +4,42 @@ interface Props {
   leaves: string[];
 }
 
-  onClick: (text: string) => void;
+function startDownload() {
+  const url =
+    'https://images.pexels.com/photos/3361739/pexels-photo-3361739.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500';
+
+  // Simulate a call to Dropbox or other service that can
+  // return an image as an ArrayBuffer.
+  let xhr = new XMLHttpRequest();
+  // Use JSFiddle logo as a sample image to avoid complicating
+  // this example with cross-domain issues.
+  xhr.open('GET', url, true);
+  // Ask for the result as an ArrayBuffer.
+  xhr.responseType = 'arraybuffer';
+  // When the image has loaded, convert it to a blob.
+  xhr.onload = function (e) {
+    const arrayBufferView = new Uint8Array(this.response);
+    parent.postMessage(
+      { pluginMessage: { type: 'update-image-node', bytes: arrayBufferView } },
+      '*'
+    );
+
+    // This can be useful to append the image to the DOM.
+    // let blob = new Blob([arrayBufferView], { type: "image/jpeg" });
+    // let urlCreator = window.URL || window.webkitURL;
+    // let imageUrl = urlCreator.createObjectURL(blob);
+  };
+
+  xhr.send();
 }
 
 const TreeLeaves: React.FC<Props> = ({ leaves }) => {
-const TreeLeaves: React.FC<Props> = ({ leaves, onClick }) => {
+  const handleImageClick = () => {
+    startDownload();
+  };
 
   const handleClick = (text: string) => {
-    parent.postMessage({ pluginMessage: { type: 'update-nodes', text } }, '*');
+    parent.postMessage({ pluginMessage: { type: 'update-text-node', text } }, '*');
   };
 
   return (
