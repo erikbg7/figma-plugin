5ca1f0d6b35aed3e9bdc9325c4bda915437a7920
erikbg7 <erik.bg7@gmail.com>
2022-11-24 12:27:14 +0100

    ✨ Selectable categories

--- ui/components/CategorySelector.tsx
+++ ui/components/CategorySelector.tsx
@@ -1,12 +1,26 @@
+import clsx from 'clsx';
 import React from 'react';
 
 const CATEGORIES = ['Channels', 'Microapps', 'Cards', 'Games', 'Music'];
 
 const CategorySelector = () => {
+  const [selected, setSelected] = React.useState('Channels');
+
   return (
     <div className="flex flex-wrap justify-center my-2">
-      {CATEGORIES.map((c) => (
-        <button className="bg-sky-200 m-1 p-2 rounded-md">{c}</button>
+      {CATEGORIES.map((cat) => (
+        <button
+          key={cat}
+          onClick={() => setSelected(cat)}
+          className={clsx(
+            'bg-sky-200 m-1 p-2 rounded-md transition-all border-2 border-transparent',
+            {
+              'border-sky-500': cat === selected,
+            }
+          )}
+        >
+          {cat}
+        </button>
       ))}
     </div>
   );
